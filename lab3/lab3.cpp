92c6455f3d7859acef886f41372cea13575fe3b4
Vladimir <58123600+flakeed@users.noreply.github.com>
2022-04-13 00:26:39 +0300

    Update lab3.cpp

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,1 +1,22 @@
-//
+#include <iostream>
+#include <stack>
+#include <queue>
+
+int main(){
+    std::stack<int> stackk;
+    std::queue<int> queuee;
+    int x,temp;
+    std::cin >> x;
+    for (int i=1; i<x;i++)
+        if (i % 7 == 0) {stackk.push(i);}
+    else{queuee.push(i);}
+    while(!stackk.empty()){
+        temp = stackk.top();
+        std::cout <<"Stack:\n"<<temp << " "<<std::endl;
+        stackk.pop();}
+    while(!queuee.empty()){
+        temp = queuee.front();
+        if(temp>10){std::cout <<"Queue:\n"<<queuee.front() << " "<<std::endl;}
+        queuee.pop();}
+    return 0;
+}
