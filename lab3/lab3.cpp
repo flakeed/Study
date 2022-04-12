0b42bcfc55d4ef2eb43a12064195dec18fe0634d
Vladimir <58123600+flakeed@users.noreply.github.com>
2022-04-13 00:50:50 +0300

    Update lab3.cpp

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -8,15 +8,18 @@ int main(){
     int x,temp;
     std::cin >> x;
         for (int i=1; i<x;i++){
-    if (i % 7 == 0) {stackk.push(i);}
-    else{queuee.push(i);}}
-    while(!stackk.empty()){
-        temp = stackk.top();
-        std::cout <<"Stack:\n"<<temp << " "<<std::endl;
-        stackk.pop();}
-    while(!queuee.empty()){
-        temp = queuee.front();
-    if(temp>10){std::cout <<"Queue:\n"<<queuee.front() << " "<<std::endl;}
-        queuee.pop();}
+    if (i % 7 == 0) {stackk.push(i);}//here we are pushing the number to the stack and check that stack %7==0
+    else{queuee.push(i);}}//here we are pushing the number to the queue, and if else we a check that queue %7==1
+    while(!stackk.empty()){//here we are popping the elements from the stack and check if it not empty
+        std::cout <<"Stack:\n"<<stackk.top() << " "<<std::endl;//here we are printing the elements from the stack
+        stackk.pop();}//here we are popping the top element of the stack
+    while(!queuee.empty()){//if element is not empty then we are popping the elements from the queue
+        temp = queuee.front();//here we are taking the front element of the queue
+    if(temp>10){std::cout <<"Queue:\n"<<temp << " "<<std::endl;}//here we are printing the elements from the queue
+        queuee.pop();}//here we are popping the front element of the queue
     return 0;
 }
+
+
+
+
